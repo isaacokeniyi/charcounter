f9f8710f0e7e2f8ca9662777f09f90e07998f264
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-12-09 22:34:42 +0000

    word counting functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const text = document.querySelector("#text");
 const counter = document.querySelector("#counter");
 
 const countChar = () => {
-  const count = text.value.length;
-  counter.textContent = `Characters: ${count}`;
+  const charCount = text.value.length;
+  const wordCount = text.value.trim().split(" ").length;
+  counter.textContent = `Characters: ${charCount} | Words: ${wordCount}`;
 };
