d9a597ce94604aae2b612329cb139293db20da60
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-12-09 22:53:51 +0000

    not counting space functionality

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const text = document.querySelector("#text");
 const counter = document.querySelector("#counter");
+const countBtn = document.querySelector("#countBtn");
+let countSpace = true;
 
 const countChar = () => {
-  const charCount = text.value.length;
+  const charCount = countSpace ? text.value.length : text.value.replaceAll(" ", "").length;
   const wordCount = text.value === "" ? 0 : text.value.trim().split(" ").length;
   counter.textContent = `Characters: ${charCount} | Words: ${wordCount}`;
 };
+
+countBtn.addEventListener("click", () => {
+  countSpace = !countSpace;
+  countBtn.textContent = `${countSpace ? "Don't" : ""} Count Spaces`;
+  countChar();
+});
