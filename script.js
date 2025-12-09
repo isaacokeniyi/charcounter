7b87b270f9ed9f62945e12dbd5796f0972ff09e4
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-12-09 22:35:12 +0000

    debug: word count at 1 when empty

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const counter = document.querySelector("#counter");
 
 const countChar = () => {
   const charCount = text.value.length;
-  const wordCount = text.value.trim().split(" ").length;
+  const wordCount = text.value === "" ? 0 : text.value.trim().split(" ").length;
   counter.textContent = `Characters: ${charCount} | Words: ${wordCount}`;
 };
