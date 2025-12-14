da1a7b47193be32735078f077c32d9e6b84a052e
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-12-14 21:10:04 +0000

    retain text on reload

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const theme = document.querySelector("#theme");
 const all = document.querySelectorAll("html *");
 const deviceTheme = document.querySelector("#device-theme");
 
+window.addEventListener("load", () => {
+  const currentText = localStorage.getItem("text");
+  if (currentText) {
+    text.value = currentText;
+  }
+});
+
 const countChar = () => {
   const charCount = text.value.replaceAll(/\n/g, "").length;
   const charCountWithoutSpaces = text.value.replaceAll(/\s/g, "").length;
@@ -24,6 +31,7 @@ const switchTheme = () => {
   switch (theme.value) {
     case "device":
       deviceTheme.disabled = false;
+      localStorage.setItem("text", text.value);
       window.location.reload();
       break;
     case "light":
